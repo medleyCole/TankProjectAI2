c66f81c21d1a0b6ed5b30bf93b9f371116c24187
Gourd (cole) <cmedley@uci.edu>
2020-03-07 17:13:26 -0800

    added in  fire variance and random redirection

--- TankYou/Assets/Scripts/PlayerController.cs
+++ TankYou/Assets/Scripts/PlayerController.cs
@@ -20,15 +20,22 @@ public class PlayerController : MonoBehaviour
     public KeyCode Down;
     public KeyCode Shoot;
     public int team;
-    
+    public float switchTimeRangeHigh;
+
     public Boundary boundary;
     private Rigidbody RB;
 
     public GameObject shot;
     public Transform shotSpawn;
     public float fireRate;
+    //makes things a little random, lowers chances of ties
+    public float fireRateVarience;
     private float nextFire;
 
+    //this controlls when the tank radomly goes off in a new diretion so the match... actually ends.
+    private float nextSwitch;
+
+    //helps make the collisions spaced out
     private float nextCollision;
 
     [SerializeField] private Transform player;
@@ -45,16 +52,19 @@ public class PlayerController : MonoBehaviour
         RB.rotation = Quaternion.Euler(moveRotation);
 
         nextCollision = 0;
-
-        if (RB)
-        {
-          //  print("have the rigidbody!");
-        }
+        nextSwitch = Random.Range(0, switchTimeRangeHigh);
     }
 
     void Update()
     {
         move();
+        if(Time.time >= nextSwitch)
+        {
+            float randomRot = Random.Range(-180, 179);
+            moveRotation = new Vector3(0.0f, randomRot, 0.0f);
+            RB.rotation = Quaternion.Euler(moveRotation);
+            nextSwitch = Random.Range(0, switchTimeRangeHigh) + Time.time;
+        }
     }
 
     void FixedUpdate ()
@@ -71,7 +81,7 @@ public class PlayerController : MonoBehaviour
 
     private void fire_shot()
     {
-            nextFire = Time.time + fireRate;
+            nextFire = Time.time + fireRate + Random.Range(0,fireRateVarience);
             Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
     }
 
@@ -82,7 +92,7 @@ public class PlayerController : MonoBehaviour
             if (other.gameObject.GetComponent<PlayerController>().team != team)
             {
                 
-                //print("pew");
+                //fire a shot when the time passes
                 if (Time.time >= nextFire + Random.Range(0,4))
                 {
                     fire_shot();
@@ -99,14 +109,15 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter(Collision other)
     {
+        //handles bullet collisions
         if (other.collider.tag == "bullet")
         {
-            print("bullet hit");
             if (other.gameObject.GetComponent<Mover>().team != team)
                 Destroy(other.gameObject);
             Destroy(this.gameObject);
         }
 
+        //handles boundry and other tank collisions 
         if((other.collider.tag == "player" || other.collider.tag == "boundary") && Time.time >= nextCollision)
         {
             RB.rotation *= Quaternion.Euler(0, 180, 0);
